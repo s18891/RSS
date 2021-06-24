e1f0bcf962d858997dc1b584d64d091804981843
Bogusław Stępień <s18891@pjwstk.edu.pl>
2021-06-24 02:28:21 +0200

    Auth

---
+++ app/src/main/java/com/pjatk/rss/RssActivity.kt
@@ -0,0 +1,11 @@
+package com.pjatk.rss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RssActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rss)
+    }
+}
